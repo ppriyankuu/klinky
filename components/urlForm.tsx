ca7b46abecb0287aa5262767076cdf61dd94bddf
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-06-06 21:20:18 +0530

    files added

--- components/urlForm.tsx
+++ components/urlForm.tsx
@@ -81,7 +81,8 @@ export const URLForm = () => {
             Enter the link
           </label>
           <input
-            type="email"
+            type="text"
+            value={inputLink}
             onChange={(e) => setInputLink(e.target.value)}
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
