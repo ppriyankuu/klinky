24e4ca10bb8c6ca9a068c1d860e15b582b035f16
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-06-07 10:41:17 +0530

    files added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import { URLForm } from '@/components/urlForm';
+import logo from '../public/logo.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex flex-col py-20 items-center justify-center w-[60%] mx-auto">
+      <div className="flex font-mono font-bold items-center gap-1">
+        <Image className="rounded-lg" src={logo} width={50} alt="" />
+        Klinky
+      </div>
       <URLForm />
     </div>
   );
